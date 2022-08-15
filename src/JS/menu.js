15a1dfdd61c728d94107ef531d798a1996d14181
srjCAJIO <serjsalo.work@gmail.com>
2022-08-15 19:23:42 +0300

    krivoj read more (

--- src/JS/menu.js
+++ src/JS/menu.js
@@ -12,3 +12,25 @@
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
+
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('.menu-open-btn'),
+//     closeMenuBtn: document.querySelector('.menu-close-btn'),
+//     menu: document.querySelector('.mob-menu'),
+//     body: document.querySelector('body'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-open');
+//     refs.body.classList.toggle('no-scroll');
+//   }
+// })();
+
+// $('.mob-menu__item a').click(function () {
+//   $('.mob-menu.is-open').removeClass('is-open');
+// });
